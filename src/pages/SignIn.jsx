732dc0999856d6e07cf17ad29646ca140215153b
Alexander Popov <shurikation@gmail.com>
2022-04-17 22:20:18 +0400

    #14.10 - Google OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRig
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {toast} from 'react-toastify'
+import OAuth from '../components/OAuth';
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -72,6 +73,8 @@ function SignIn() {
             </button>
           </div>
         </form>
+
+        <OAuth />
 
         <Link to='/sign-up' className='registerLink'>
           Sign Up Instead
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
-import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
-import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
+import OAuth from '../components/OAuth';
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -27,22 +28,22 @@ function SignUp() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
- 
+
     try {
       const auth = getAuth();
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
       updateProfile(auth.currentUser, {
-        displayName: name, 
+        displayName: name,
       })
 
-      const formDataCopy = {...formData};
+      const formDataCopy = { ...formData };
       delete formDataCopy.password
       formDataCopy.timestamp = serverTimestamp()
       await setDoc(doc(db, 'users', user.uid), formDataCopy)
       navigate('/')
-    } catch(error) {
-        toast.error('Something wrong!')
+    } catch (error) {
+      toast.error('Something wrong!')
     }
   }
 
@@ -86,6 +87,8 @@ function SignUp() {
           </div>
         </form>
 
+        <OAuth />
+        
         <Link to='/sign-in' className='registerLink'>
           Sign Up Instead
         </Link>
